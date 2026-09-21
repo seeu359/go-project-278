d7a27851c625237be28952cb1490fb19a19aa3cc
Aleksei Cheryomushkin <a.cheryomushkin@revo.com>
2026-09-21 20:22:45 +0300

    add sqlc

---
+++ query.sql
@@ -0,0 +1,21 @@
+-- name: GetLinks :many
+SELECT * FROM links;
+
+-- name: GetLink :one
+SELECT * FROM links WHERE id = $1;
+
+-- name: CreateLink :one
+INSERT INTO links (url, short_name)
+VALUES ($1, $2)
+RETURNING id;
+
+-- name: UpdateLink :execresult
+UPDATE links
+SET url = $1,
+    short_name = $2
+WHERE id = $3;
+
+-- name: DeleteLink :execresult
+DELETE FROM links
+WHERE id = $1;
+
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE links (
+  id   BIGSERIAL PRIMARY KEY,
+  url TEXT  NOT NULL,
+  short_name TEXT NOT NULL
+);
